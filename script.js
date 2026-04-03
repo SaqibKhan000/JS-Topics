2af08be69f82ce454703866a7b7323e1532c354b
M Saqib Khan <saqibkhannn543@gmail.com>
2026-04-03 18:36:16 +0500

    Filter and find method

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 
 
+const users = [
+    { name: 'Ali', address: 'Karachi' },
+    { name: 'Saqib', address: 'Peshawar' },
+    { name: 'Haider', address: 'Islamabad' },
+    { name: 'Jamal', address: 'Peshawar' }
+]
 
-
-console.log(Object.is(+0, -0))
+const arr = users.find((user) => {
+    return user.address === 'Peshawar';
+})
+console.log(arr);
