68a740bc8485cd3f7e56853dc1d015f8fd789e5f
M Saqib Khan <saqibkhannn543@gmail.com>
2026-04-05 16:36:16 +0500

    The reason behind -1

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 
 
-const users = [
-    { name: 'Ali', address: 'Karachi' },
-    { name: 'Saqib', address: 'Peshawar' },
-    { name: 'Haider', address: 'Islamabad' },
-    { name: 'Jamal', address: 'Peshawar' }
-]
 
-const arr = users.find((user) => {
-    return user.address === 'Peshawar';
-})
-console.log(arr);
+const arr = [10, 20, 30, 40];
+
+console.log(arr.indexOf(50));
